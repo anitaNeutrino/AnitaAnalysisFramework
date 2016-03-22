615d764a79da793bf038a2ff90b5b600f24a0060
strutt <strutt@d90a992a-4ffb-0310-9b0e-ce574b03d632>
2016-03-22 13:20:42 +0000

    After a long afternoon trying to debug computer specific ROOT errors, checking out an old version has magically fixed everything. Thank goodness for version control.
    git-svn-id: https://delos.mps.ohio-state.edu/anitaGround/AnitaAnalysis@3583 d90a992a-4ffb-0310-9b0e-ce574b03d632

--- include/AnitaEventSummary.h
+++ include/AnitaEventSummary.h
@@ -9,90 +9,99 @@
 
 class AnitaEventSummary 
 {
+public: 
+  static const Int_t maxDirectionsPerPol = 3; 
+
+  class PointingHypothesis 
+  {
   public: 
-    static const UInt_t MaxPointingHypotheses = 6; 
+    Double_t phi;  // peak phi, degrees
+    Double_t theta; // peak theta, degrees
+    Double_t value; // peak value
+    Double_t snr; // snr of peak
+    Double_t hwAngle; // angle with respect to triggering phi sector
+    Bool_t triggered; 
+    Bool_t masked; 
 
-    class PointingHypothesis 
-    {
-      public: 
-        Double_t phi;  // peak phi, degrees
-        Double_t theta; // peak theta, degrees
-        Double_t value; // peak value
-        Double_t snr; // snr of peak
-        Double_t hwAngle; // angle with respect to triggering phi sector
-        Bool_t triggered; 
-        Bool_t masked; 
-
-        /* covariance matrix?*/ 
+    Double_t latitude;
+    Double_t longitude;
+    Double_t altitude;
+    
+    /* covariance matrix?*/ 
     //    Double_t var_theta;  
     //    Double_t var_phi; 
     //    Double_t covar; 
 
-        virtual ~PointingHypothesis() {;}
-      ClassDef(PointingHypothesis,1); 
-    }; 
+    virtual ~PointingHypothesis() {;}
+    ClassDef(PointingHypothesis,2); 
+  }; 
 
   class WaveformInfo
   {
-    public: 
-      Double_t snr; 
-      Double_t peakHilbert; 
-      Double_t peakVal; 
-      Double_t bandwidth; 
-      virtual ~WaveformInfo() {; } 
+  public: 
+    Double_t snr; 
+    Double_t peakHilbert; 
+    Double_t peakVal; 
+    Double_t bandwidth;
+    Int_t numAntennasInCoherent;     
+    virtual ~WaveformInfo() {; } 
 
-      ClassDef(WaveformInfo, 1); 
+    ClassDef(WaveformInfo, 1); 
   }; 
 
-   class EventFlags
-    {
-      public: 
-        enum CalPulser
-        {
-          NONE, 
-          WAIS, 
-          LDB, 
-          SIPLE,
-          TD
-        }; 
+  class EventFlags
+  {
+  public: 
+    enum CalPulser
+      {
+	NONE, 
+	WAIS, 
+	LDB, 
+	SIPLE,
+	TD
+      }; 
 
-        Int_t isGood; 
-        Int_t isRF; 
-        Int_t isSoftwareTrigger; 
-        Int_t isPayloadBlast; 
-        Int_t nadirFlag; 
-        Int_t strongCWFlag; 
+    Int_t isGood; 
+    Int_t isRF; 
+    Int_t isSoftwareTrigger; 
+    Int_t isPayloadBlast; 
+    Int_t nadirFlag; 
+    Int_t strongCWFlag;
+    Int_t isHPolTrigger;
+    Int_t isVPolTrigger;
 
-        CalPulser pulser; 
-        Bool_t isVarner; 
-        Bool_t isVarner2; 
-        virtual ~EventFlags() {;}
+    CalPulser pulser; 
+    Bool_t isVarner; 
+    Bool_t isVarner2; 
+    virtual ~EventFlags() {;}
 
-      ClassDef(EventFlags,1); 
-    }; 
+    ClassDef(EventFlags,2); 
+  }; 
 
 
-    UInt_t runNumber; 
-    UInt_t eventNumber; 
+  Int_t run; 
+  UInt_t eventNumber; 
+  
+  PointingHypothesis peak[AnitaPol::kNotAPol][maxDirectionsPerPol]; 
+  WaveformInfo coherent[AnitaPol::kNotAPol][maxDirectionsPerPol];
+  WaveformInfo deconvolved[AnitaPol::kNotAPol][maxDirectionsPerPol];
+  // WaveformInfo maxWaveform;  // do we want this for all ? 
 
-    PointingHypothesis hypotheses[MaxPointingHypotheses]; 
-    UInt_t nantennas_in_coherent; 
-    WaveformInfo coherent[MaxPointingHypotheses]; 
-    WaveformInfo deconvolved[MaxPointingHypotheses]; 
-    WaveformInfo maxWaveform;  // do we want this for all ? 
+  EventFlags flags; 
+  // Adu5Pat gps;
+  // RawAnitaHeader header;
 
-    EventFlags flags; 
-    Adu5Pat gps; 
-    RawAnitaHeader header; 
 
   AnitaEventSummary();
-  AnitaEventSummary(const RawAnitaHeader* header, const Adu5Pat* pat);
+  AnitaEventSummary(const RawAnitaHeader* header);//, const Adu5Pat* pat);
+  void tagTriggerAsHPolOrVPol(const RawAnitaHeader* header);
+  void zeroInternals();
   virtual ~AnitaEventSummary() { ; } 
 
   
-  private: 
+private: 
 
-    ClassDef(AnitaEventSummary, 2); 
+  ClassDef(AnitaEventSummary, 3); 
 }; 
 
 
--- src/AnitaEventSummary.cc
+++ src/AnitaEventSummary.cc
@@ -10,7 +10,7 @@ ClassImp(AnitaEventSummary)
  * Default constructor for ROOT
  */
 AnitaEventSummary::AnitaEventSummary(){
-  
+  zeroInternals();
 }
 
 
@@ -21,10 +21,84 @@ AnitaEventSummary::AnitaEventSummary(){
  *
  * Takes care of copying the header and GPS info into the event summary
  */
-AnitaEventSummary::AnitaEventSummary(const RawAnitaHeader* head,
-				     const Adu5Pat* pat){
+AnitaEventSummary::AnitaEventSummary(const RawAnitaHeader* header){
 
-  this->header = (*head);
-  this->gps = (*pat);  
+  zeroInternals();
+
+  tagTriggerAsHPolOrVPol(header);
+  eventNumber = header->eventNumber;
+  run = header->run;
+}
+
+
+void AnitaEventSummary::zeroInternals(){
+
+  run = 0;
+  eventNumber = 0;
+
+  for(Int_t polInd=0; polInd < AnitaPol::kNotAPol; polInd++){
+    for(Int_t dir=0; dir < maxDirectionsPerPol; dir++){
+      peak[polInd][dir].value = 0;
+      peak[polInd][dir].theta = 0;
+      peak[polInd][dir].value = 0;
+      peak[polInd][dir].snr = 0;
+      peak[polInd][dir].hwAngle = 0;
+      peak[polInd][dir].triggered = 0;
+      peak[polInd][dir].masked = 0;
+      peak[polInd][dir].latitude = 0;
+      peak[polInd][dir].longitude = 0;
+      peak[polInd][dir].altitude = 0;
+
+      coherent[polInd][dir].snr = 0; 
+      coherent[polInd][dir].peakHilbert = 0; 
+      coherent[polInd][dir].peakVal = 0; 
+      coherent[polInd][dir].bandwidth = 0;
+      coherent[polInd][dir].numAntennasInCoherent = 0;
+      
+      deconvolved[polInd][dir].snr = 0; 
+      deconvolved[polInd][dir].peakHilbert = 0; 
+      deconvolved[polInd][dir].peakVal = 0; 
+      deconvolved[polInd][dir].bandwidth = 0;
+      deconvolved[polInd][dir].numAntennasInCoherent = 0;      
+    }
+  }
+
+  flags.isGood = 0;
+  flags.isRF = 0;
+  flags.isSoftwareTrigger = 0;
+  flags.isPayloadBlast = 0; 
+  flags.nadirFlag = 0; 
+  flags.strongCWFlag = 0;
+  flags.isHPolTrigger = 0;
+  flags.isVPolTrigger = 0;
+  flags.pulser = EventFlags::NONE; 
+  flags.isVarner = 0; 
+  flags.isVarner2 = 0; 
   
+  
+}
+
+
+void AnitaEventSummary::tagTriggerAsHPolOrVPol(const RawAnitaHeader* header){
+
+  Int_t numL3sV = 0;
+  Int_t numL3sH = 0;  
+  for(Int_t phi=0; phi<NUM_PHI; phi++){
+    
+    if(((header->l3TrigPattern >> phi) & 1) > 0){
+      numL3sV++;
+    }
+    if(((header->l3TrigPatternH >> phi) & 1) > 0){
+      numL3sH++;
+    }    
+  }
+
+  const int numL3TriggersForEvent = 2;
+  if(numL3sV >= numL3TriggersForEvent){
+    flags.isVPolTrigger = 1;
+  }
+  if(numL3sH >= numL3TriggersForEvent){
+    flags.isHPolTrigger = 1;
+  }
+
 }
