b013cd33754a33bf21dfb11effa7980ed5fa54a0
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2016-09-26 01:12:08 -0500

    make root 6 compatible

--- macros/testWaveform.C
+++ macros/testWaveform.C
@@ -1,9 +1,10 @@
 
+#include "FFTtools.h" 
+#include "AnalyticSignal.h" 
+#include "AnalysisWaveform.h" 
+
+void testWaveform() 
 {
-
-  gSystem->Load("libRootFftwWrapper.so"); 
-  gSystem->Load("lib/libAnitaAnalysis.so"); 
-
   FFTtools::ThermalNoise noise(5000, 0.2/1.3, 1.2/1.3, 10,2); 
   noise.setExtraNoise(0.5); 
 
@@ -33,7 +34,7 @@
   sig.drawHilbertEnvelope(); 
   
 
-  TCanvas ccorr("ccorr","ccorr",800,800) ; 
+  TCanvas* ccorr = new TCanvas("ccorr","ccorr",800,800) ; 
 
 
 
@@ -44,41 +45,41 @@
   for (int i = 0; i < 256; i++) 
   {
     g2->GetY()[i] += 10*sin(2 * TMath::Pi() * (g2->GetX()[i] * 0.1 + 5)); 
-    g3->GetY()[i] += 10*sin(2 * TMath::Pi() * (g3->GetX()[i] *0.1 ); 
+    g3->GetY()[i] += 10*sin(2 * TMath::Pi() * (g3->GetX()[i] *0.1 )); 
     g2->GetY()[i] += 10*sin(2 * TMath::Pi() * (g2->GetX()[i] * 0.2 + 5)); 
     g3->GetY()[i] += 10*sin(2 * TMath::Pi() * (g3->GetX()[i]* 0.2 )); 
   }
 
   double scale = g2->GetRMS(2) * g2->GetRMS(2); 
-  AnalysisWaveform sig2(g2->GetN(), g2->GetX(), g2->GetY(), 1./2.6); 
-  AnalysisWaveform sig3(g3->GetN(), g3->GetX(), g3->GetY(), 1./2.6); 
+  AnalysisWaveform *sig2 = new AnalysisWaveform(g2->GetN(), g2->GetX(), g2->GetY(), 1./2.6); 
+  AnalysisWaveform *sig3 = new AnalysisWaveform(g3->GetN(), g3->GetX(), g3->GetY(), 1./2.6); 
 
-  sig3.forceEvenSize(sig2.even()->GetN()); 
+  sig3->forceEvenSize(sig2->even()->GetN()); 
 
-  ccorr.Divide(2,2); 
-  TGraph * fftcorr = FFTtools::getCorrelationGraph((TGraph*) sig2.even(), (TGraph*)sig3.even()); 
-  sig2.padEven(1); 
-  sig3.padEven(1); 
+  ccorr->Divide(2,2); 
+  TGraph * fftcorr = FFTtools::getCorrelationGraph((TGraph*) sig2->even(), (TGraph*)sig3->even()); 
+  sig2->padEven(1); 
+  sig3->padEven(1); 
   
-  ccorr.cd(1); 
-  sig2.drawEven(); 
-  ccorr.cd(2); 
-  sig3.drawEven(); 
+  ccorr->cd(1); 
+  sig2->drawEven(); 
+  ccorr->cd(2); 
+  sig3->drawEven(); 
 
-  AnalysisWaveform * corr = AnalysisWaveform::correlation(&sig2,&sig3,0,1); 
-  ccorr.cd(3); 
+  AnalysisWaveform * corr = AnalysisWaveform::correlation(sig2,sig3,0,1); 
+  ccorr->cd(3); 
   corr->drawEven(); 
 
-  ccorr.cd(4); 
+  ccorr->cd(4); 
   //AnalysisWaveform *copy = new AnalysisWaveform(*corr2); 
 
-  fftcorr.Draw(); 
+  fftcorr->Draw(); 
 
 
-  TCanvas cfreq("cfreq"); 
+  TCanvas * cfreq = new TCanvas("cfreq"); 
 
   TGraph * g10 = noise.makeGaussianDtWf(256,1./2.6,0.1); 
-  AnalysisWaveform sig10(g10->GetN(), g10->GetX(), g10->GetY(), 1./2.6); 
+  AnalysisWaveform * sig10 = new AnalysisWaveform(g10->GetN(), g10->GetX(), g10->GetY(), 1./2.6); 
 
 
   int nfreq = sig10->Nfreq(); 
@@ -94,10 +95,10 @@
   }
 
 
-  cfreq.Divide(2,1); 
-  cfreq.cd(1); 
+  cfreq->Divide(2,1); 
+  cfreq->cd(1); 
   sig10->drawPower(); 
-  cfreq.cd(2); 
+  cfreq->cd(2); 
   sig10->drawEven(); 
 
 
