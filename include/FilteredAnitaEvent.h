d298eaa5a3acada6894fc6f1c064cb89dcd82d8d
Ben Strutt <b.strutt.12@ucl.ac.uk>
2017-02-09 16:53:18 +0000

    Added eventNumber + getGraph function similar to UsefulAnitaEvent...
    To coax templates to working in CrossCorrelator in anitaAnalysisTools.

--- include/FilteredAnitaEvent.h
+++ include/FilteredAnitaEvent.h
@@ -1,96 +1,102 @@
 #ifndef _FILTERED_ANITA_EVENT_H_
 #define _FILTERED_ANITA_EVENT_H_
-#include "TObject.h" 
-#include "AnitaConventions.h" 
-#include "UsefulAdu5Pat.h" 
+#include "TObject.h"
+#include "AnitaConventions.h"
+#include "UsefulAdu5Pat.h"
+#include "AnalysisWaveform.h"
 
-class UsefulAnitaEvent; 
+class UsefulAnitaEvent;
 class Adu5Pat;
-class TGraph; 
-class RawAnitaHeader; 
-class AnalysisWaveform; 
-class TCanvas; 
+class TGraph;
+class RawAnitaHeader;
+class TCanvas;
 
-class FilterStrategy; 
+class FilterStrategy;
 
 
-/** 
+/**
   \brief
-  This class is intended to store all the necessary data about an ANITA event for filtering and analysis. 
-  It stores the raw and filtered waveforms as well as auxilliary information. 
+  This class is intended to store all the necessary data about an ANITA event for filtering and analysis.
+  It stores the raw and filtered waveforms as well as auxilliary information.
 */
 
-class FilteredAnitaEvent 
+class FilteredAnitaEvent
 {
 
-  friend class FilterOperation; 
-  friend class FilterStrategy; 
+  friend class FilterOperation;
+  friend class FilterStrategy;
 
   public:
-  
-  
-   /** Create a FilteredAnitaEvent from a useful event, a filter strategy, GPS info and the event header. If keep_all_stages is true, then  all stages of filtering are kept. Note that filtering occurs during construction, not later!*/ 
-   FilteredAnitaEvent(const UsefulAnitaEvent * event, FilterStrategy * strategy, const Adu5Pat * pat, const RawAnitaHeader * header, bool keep_all_stages = false); 
 
-   /** Empty FilteredAnitaEvent. Not particularly useful */ 
-   FilteredAnitaEvent(); 
+
+   /** Create a FilteredAnitaEvent from a useful event, a filter strategy, GPS info and the event header. If keep_all_stages is true, then  all stages of filtering are kept. Note that filtering occurs during construction, not later!*/
+   FilteredAnitaEvent(const UsefulAnitaEvent * event, FilterStrategy * strategy, const Adu5Pat * pat, const RawAnitaHeader * header, bool keep_all_stages = false);
+
+   /** Empty FilteredAnitaEvent. Not particularly useful */
+   FilteredAnitaEvent();
 
 
    /** Destructor */
-   virtual ~FilteredAnitaEvent(); 
+   virtual ~FilteredAnitaEvent();
 
-   /** Accessor for raw waveform based on index. Mostly useful if you want to iterate over everything */ 
+   /** Accessor for raw waveform based on index. Mostly useful if you want to iterate over everything */
    const AnalysisWaveform * getRawGraph(UInt_t i) const { return rawGraphs[i]; }
 
    /** Accessor for raw waveform based on antenna number and polarization. */
    const AnalysisWaveform * getRawGraph(UInt_t ant, AnitaPol::AnitaPol_t pol) const { return rawGraphsByAntPol[pol][ant]; }
 
-   /** Accessor for filtered waveform based on index. Mostly useful if you want to iterate over everything */ 
+   /** Accessor for filtered waveform based on index. Mostly useful if you want to iterate over everything */
    const AnalysisWaveform * getFilteredGraph(UInt_t i) const { return filteredGraphs[i]; }
 
    /** Accessor for filtered waveform based on antenna number and polarization. */
    const AnalysisWaveform * getFilteredGraph(UInt_t ant, AnitaPol::AnitaPol_t pol) const { return filteredGraphsByAntPol[pol][ant]; }
 
 
-   /* If the FilteredAnitaEvent was told to keep_all_stages, you can get the waveform at each stage. Stage 0 is after the first filter operation. */ 
-   const AnalysisWaveform * getFilteredGraphAtStage(UInt_t ant, AnitaPol::AnitaPol_t pol, UInt_t stage) const; 
+   /* If the FilteredAnitaEvent was told to keep_all_stages, you can get the waveform at each stage. Stage 0 is after the first filter operation. */
+   const AnalysisWaveform * getFilteredGraphAtStage(UInt_t ant, AnitaPol::AnitaPol_t pol, UInt_t stage) const;
 
    /** Return the UsefulAnitaEvent this event was constructed with. */
-   const UsefulAnitaEvent* getUsefulAnitaEvent() const { return useful; } 
-   /** Return the Useful GPS info */ 
-   const UsefulAdu5Pat * getGPS() const { return &pat; } 
+   const UsefulAnitaEvent* getUsefulAnitaEvent() const { return useful; }
+   /** Return the Useful GPS info */
+   const UsefulAdu5Pat * getGPS() const { return &pat; }
 
-   /** Return the header */ 
-   const RawAnitaHeader * getHeader() const { return header; } 
+   /** Return the header */
+   const RawAnitaHeader * getHeader() const { return header; }
 
-   void plotSummary(TCanvas * chpol = 0, TCanvas * cvpol = 0) const; 
-
-   /** Various calculations. Don't necessarily have to be in this class. */ 
-   void getAverageSpectrum (TGraph * target, AnitaPol::AnitaPol_t pol = AnitaPol::kNotAPol ) const; 
-   void getMedianSpectrum  (TGraph * target, AnitaPol::AnitaPol_t pol = AnitaPol::kNotAPol , double pctile = 0.5) const; 
-   double getAveragePower(AnitaPol::AnitaPol_t pol = AnitaPol::kNotAPol, AnitaRing::AnitaRing_t ring = AnitaRing::kNotARing, bool filtered = false) const; 
-   double getMedianPower(AnitaPol::AnitaPol_t pol = AnitaPol::kNotAPol,  AnitaRing::AnitaRing_t ring = AnitaRing::kNotARing, bool filtered = false) const; 
-   void getMinMaxRatio(AnitaPol::AnitaPol_t pol, double * max_ratio, double * min_ratio, int* max_sector, int* min_sector, AnitaRing::AnitaRing_t ring1 = AnitaRing::kBottomRing, AnitaRing::AnitaRing_t ring2 = AnitaRing::kTopRing, int nth = 0) const; 
-
-   int getAnitaVersion() const { return anitaVersion; } 
-
-  private: 
-   AnalysisWaveform *rawGraphs[NUM_SEAVEYS*AnitaPol::kNotAPol]; 
-   AnalysisWaveform *rawGraphsByAntPol[AnitaPol::kNotAPol][NUM_SEAVEYS]; 
-   AnalysisWaveform *filteredGraphs[NUM_SEAVEYS*AnitaPol::kNotAPol]; 
-   AnalysisWaveform *filteredGraphsByAntPol[AnitaPol::kNotAPol][NUM_SEAVEYS]; 
+   void plotSummary(TCanvas * chpol = 0, TCanvas * cvpol = 0) const;
 
 
-   const UsefulAnitaEvent * useful; 
-   const FilterStrategy * strategy; 
-   UsefulAdu5Pat pat; 
-   const RawAnitaHeader * header; 
+   /** Various calculations. Don't necessarily have to be in this class. */
+   void getAverageSpectrum (TGraph * target, AnitaPol::AnitaPol_t pol = AnitaPol::kNotAPol ) const;
+   void getMedianSpectrum  (TGraph * target, AnitaPol::AnitaPol_t pol = AnitaPol::kNotAPol , double pctile = 0.5) const;
+   double getAveragePower(AnitaPol::AnitaPol_t pol = AnitaPol::kNotAPol, AnitaRing::AnitaRing_t ring = AnitaRing::kNotARing, bool filtered = false) const;
+   double getMedianPower(AnitaPol::AnitaPol_t pol = AnitaPol::kNotAPol,  AnitaRing::AnitaRing_t ring = AnitaRing::kNotARing, bool filtered = false) const;
+   void getMinMaxRatio(AnitaPol::AnitaPol_t pol, double * max_ratio, double * min_ratio, int* max_sector, int* min_sector, AnitaRing::AnitaRing_t ring1 = AnitaRing::kBottomRing, AnitaRing::AnitaRing_t ring2 = AnitaRing::kTopRing, int nth = 0) const;
 
-   int anitaVersion; 
+   int getAnitaVersion() const { return anitaVersion; }
 
-   bool keep_all_stages; 
+
+  // Functions that are the same as in UsefulAnitaEvent to allow for templates in CrossCorrelator
+  TGraph* getGraph(Int_t ant, AnitaPol::AnitaPol_t pol);
+  UInt_t eventNumber;
+
+  private:
+   AnalysisWaveform *rawGraphs[NUM_SEAVEYS*AnitaPol::kNotAPol];
+   AnalysisWaveform *rawGraphsByAntPol[AnitaPol::kNotAPol][NUM_SEAVEYS];
+   AnalysisWaveform *filteredGraphs[NUM_SEAVEYS*AnitaPol::kNotAPol];
+   AnalysisWaveform *filteredGraphsByAntPol[AnitaPol::kNotAPol][NUM_SEAVEYS];
+
+
+   const UsefulAnitaEvent * useful;
+   const FilterStrategy * strategy;
+   UsefulAdu5Pat pat;
+   const RawAnitaHeader * header;
+
+   int anitaVersion;
+
+   bool keep_all_stages;
    std::vector<AnalysisWaveform *> all_stages[AnitaPol::kNotAPol][NUM_SEAVEYS] ;
-   void saveStage(int nreserve); 
+   void saveStage(int nreserve);
 };
 
 
@@ -98,4 +104,3 @@ class FilteredAnitaEvent
 
 
 #endif
-
--- src/FilteredAnitaEvent.cc
+++ src/FilteredAnitaEvent.cc
@@ -1,64 +1,72 @@
-#include "FilteredAnitaEvent.h" 
+#include "FilteredAnitaEvent.h"
 #include "UsefulAnitaEvent.h"
 #include "FilterStrategy.h"
 #include "RawAnitaHeader.h"
 #include "AnalysisWaveform.h"
-#include "AnitaVersion.h" 
+#include "AnitaVersion.h"
 #include "AnitaGeomTool.h"
 #include <algorithm>
-#include "TCanvas.h" 
-#include "TStyle.h" 
+#include "TCanvas.h"
+#include "TStyle.h"
 
 
-//ClassImp(FilteredAnitaEvent); 
+//ClassImp(FilteredAnitaEvent);
 
-FilteredAnitaEvent::FilteredAnitaEvent() 
+FilteredAnitaEvent::FilteredAnitaEvent()
 {
   for (unsigned i = 0; i < 2 * NUM_SEAVEYS; i++ )
     {
-    filteredGraphs[i] = NULL; 
-    rawGraphs[i] = NULL; 
+    filteredGraphs[i] = NULL;
+    rawGraphs[i] = NULL;
   }
+  eventNumber = 0;
 }
 
-FilteredAnitaEvent:: FilteredAnitaEvent(const UsefulAnitaEvent * event, FilterStrategy * strategy, const Adu5Pat * rawpat, const RawAnitaHeader * header, bool save_stages ) 
-  : useful(event), 
-    strategy(strategy), 
-    pat(rawpat), 
+FilteredAnitaEvent:: FilteredAnitaEvent(const UsefulAnitaEvent * event, FilterStrategy * strategy, const Adu5Pat * rawpat, const RawAnitaHeader * header, bool save_stages )
+  : useful(event),
+    strategy(strategy),
+    pat(rawpat),
     header(header),
     keep_all_stages(save_stages)
-
 {
+  eventNumber = event->eventNumber;
 
-#ifdef MULTIVERSION_ANITA_ENABLED  
-  anitaVersion = AnitaVersion::getVersionFromUnixTime(header->realTime); 
-#else 
-  anitaVersion = 0; 
-#endif 
+#ifdef MULTIVERSION_ANITA_ENABLED
+  anitaVersion = AnitaVersion::getVersionFromUnixTime(header->realTime);
+#else
+  anitaVersion = 0;
+#endif
 
-  if (anitaVersion <= 0) anitaVersion = AnitaVersion::get(); 
+  if (anitaVersion <= 0) anitaVersion = AnitaVersion::get();
 
-  AnitaGeomTool * geom = AnitaGeomTool::Instance(); 
-  // Initialize the filtered graphs with the raw graphs from Raw Anita Event 
-  
+  AnitaGeomTool * geom = AnitaGeomTool::Instance();
+  // Initialize the filtered graphs with the raw graphs from Raw Anita Event
+
   for (int pol = AnitaPol::kHorizontal; pol <= AnitaPol::kVertical; pol++)
   {
-    for (unsigned ant = 0; ant < NUM_SEAVEYS; ant++) 
+    for (unsigned ant = 0; ant < NUM_SEAVEYS; ant++)
     {
-      int k = pol * NUM_SEAVEYS  + ant; 
-      int i = geom->getChanIndexFromAntPol(ant, (AnitaPol::AnitaPol_t) pol); 
-      filteredGraphs[k] = new AnalysisWaveform (useful->fNumPoints[i], useful->fTimes[i], useful->fVolts[i]); 
-      rawGraphs[k] = new AnalysisWaveform (useful->fNumPoints[i], useful->fTimes[i], useful->fVolts[i]); 
-      filteredGraphs[k]->forceEvenSize(260); // do this for correlations 
-//      rawGraphs[k]->forceEvenSize(260); // do this for correlations 
-      filteredGraphsByAntPol[pol][ant] = filteredGraphs[k];  
-      rawGraphsByAntPol[pol][ant] = rawGraphs[k];  
+      int k = pol * NUM_SEAVEYS  + ant;
+      int i = geom->getChanIndexFromAntPol(ant, (AnitaPol::AnitaPol_t) pol);
+      filteredGraphs[k] = new AnalysisWaveform (useful->fNumPoints[i], useful->fTimes[i], useful->fVolts[i]);
+      rawGraphs[k] = new AnalysisWaveform (useful->fNumPoints[i], useful->fTimes[i], useful->fVolts[i]);
+      filteredGraphs[k]->forceEvenSize(260); // do this for correlations
+//      rawGraphs[k]->forceEvenSize(260); // do this for correlations
+      filteredGraphsByAntPol[pol][ant] = filteredGraphs[k];
+      rawGraphsByAntPol[pol][ant] = rawGraphs[k];
     }
   }
 
 
   //tell the strategy to process this
-  strategy->process(this); 
+  strategy->process(this);
+}
+
+
+TGraph* FilteredAnitaEvent::getGraph(Int_t ant, AnitaPol::AnitaPol_t pol){
+  const AnalysisWaveform* aw = getFilteredGraph(ant, pol);
+  const TGraph* gr = aw->even();
+  return (TGraph*) gr->Clone();
 }
 
 
@@ -67,12 +75,12 @@ const AnalysisWaveform * FilteredAnitaEvent::getFilteredGraphAtStage(UInt_t ant,
 {
   if (!keep_all_stages)
   {
-    fprintf(stderr,"You didn't ask to save all the stages!\n"); 
-    return 0; 
+    fprintf(stderr,"You didn't ask to save all the stages!\n");
+    return 0;
   }
 
-  if (stage >= all_stages[pol][ant].size()) return filteredGraphsByAntPol[pol][ant]; 
-  else return all_stages[pol][ant][stage]; 
+  if (stage >= all_stages[pol][ant].size()) return filteredGraphsByAntPol[pol][ant];
+  else return all_stages[pol][ant][stage];
 }
 
 
@@ -83,24 +91,24 @@ void FilteredAnitaEvent::saveStage(int nreserve)
   {
     for (int ant = 0; ant < NUM_SEAVEYS; ant++)
     {
-      all_stages[pol][ant].reserve(nreserve); 
-      all_stages[pol][ant].push_back(new AnalysisWaveform(*filteredGraphsByAntPol[pol][ant])); 
+      all_stages[pol][ant].reserve(nreserve);
+      all_stages[pol][ant].push_back(new AnalysisWaveform(*filteredGraphsByAntPol[pol][ant]));
     }
   }
 }
 
-FilteredAnitaEvent::~FilteredAnitaEvent() 
+FilteredAnitaEvent::~FilteredAnitaEvent()
 {
   for (unsigned pol = 0; pol < 2; pol++)
   {
     for (unsigned ant = 0; ant <  NUM_SEAVEYS; ant++ )
     {
-      delete filteredGraphsByAntPol[pol][ant]; 
-      delete rawGraphsByAntPol[pol][ant];  
+      delete filteredGraphsByAntPol[pol][ant];
+      delete rawGraphsByAntPol[pol][ant];
 
       for (unsigned j = 0; j < all_stages[pol][ant].size() ; j++)
       {
-        delete all_stages[pol][ant][j]; 
+        delete all_stages[pol][ant][j];
       }
     }
   }
@@ -110,24 +118,24 @@ FilteredAnitaEvent::~FilteredAnitaEvent()
 double FilteredAnitaEvent::getAveragePower(AnitaPol::AnitaPol_t pol, AnitaRing::AnitaRing_t ring, bool filtered) const
 {
 
-  AnitaPol::AnitaPol_t pol_start =pol == AnitaPol::kVertical ? AnitaPol::kVertical : AnitaPol::kHorizontal;  
-  AnitaPol::AnitaPol_t pol_end = pol == AnitaPol::kHorizontal ? AnitaPol::kHorizontal : AnitaPol::kVertical; 
+  AnitaPol::AnitaPol_t pol_start =pol == AnitaPol::kVertical ? AnitaPol::kVertical : AnitaPol::kHorizontal;
+  AnitaPol::AnitaPol_t pol_end = pol == AnitaPol::kHorizontal ? AnitaPol::kHorizontal : AnitaPol::kVertical;
 
-  double sum = 0; 
+  double sum = 0;
 
-  int n = NUM_SEAVEYS * (int(pol_end) - int(pol_start) +1); 
-  if (ring != AnitaRing::kNotARing) n/=AnitaRing::kNotARing; 
+  int n = NUM_SEAVEYS * (int(pol_end) - int(pol_start) +1);
+  if (ring != AnitaRing::kNotARing) n/=AnitaRing::kNotARing;
 
-  for (int pol = (int) pol_start; pol <=(int) pol_end; pol++ ) 
+  for (int pol = (int) pol_start; pol <=(int) pol_end; pol++ )
   {
-    for (int ant = 0; ant < NUM_SEAVEYS; ant++) 
+    for (int ant = 0; ant < NUM_SEAVEYS; ant++)
     {
-      if (ring != AnitaRing::kNotARing && AnitaGeomTool::getRingFromAnt(ant) != ring) continue; 
-      sum += ( filtered ?  filteredGraphsByAntPol[pol][ant]->even() : rawGraphsByAntPol[pol][ant]->uneven())->getSumV2(); 
+      if (ring != AnitaRing::kNotARing && AnitaGeomTool::getRingFromAnt(ant) != ring) continue;
+      sum += ( filtered ?  filteredGraphsByAntPol[pol][ant]->even() : rawGraphsByAntPol[pol][ant]->uneven())->getSumV2();
     }
   }
 
-  return sum/n; 
+  return sum/n;
 
 }
 
@@ -135,91 +143,91 @@ double FilteredAnitaEvent::getAveragePower(AnitaPol::AnitaPol_t pol, AnitaRing::
 double FilteredAnitaEvent::getMedianPower(AnitaPol::AnitaPol_t pol, AnitaRing::AnitaRing_t ring, bool filtered) const
 {
 
-  AnitaPol::AnitaPol_t pol_start =pol == AnitaPol::kVertical ? AnitaPol::kVertical : AnitaPol::kHorizontal;  
-  AnitaPol::AnitaPol_t pol_end = pol == AnitaPol::kHorizontal ? AnitaPol::kHorizontal : AnitaPol::kVertical; 
+  AnitaPol::AnitaPol_t pol_start =pol == AnitaPol::kVertical ? AnitaPol::kVertical : AnitaPol::kHorizontal;
+  AnitaPol::AnitaPol_t pol_end = pol == AnitaPol::kHorizontal ? AnitaPol::kHorizontal : AnitaPol::kVertical;
 
-  int n = NUM_SEAVEYS * (int(pol_end) - int(pol_start) +1); 
+  int n = NUM_SEAVEYS * (int(pol_end) - int(pol_start) +1);
 
-  if (ring != AnitaRing::kNotARing) n/=AnitaRing::kNotARing; 
+  if (ring != AnitaRing::kNotARing) n/=AnitaRing::kNotARing;
 
-  double vals[n]; 
+  double vals[n];
 
 
-  int i = 0; 
-  for (int pol = (int) pol_start; pol <=(int) pol_end; pol++ ) 
+  int i = 0;
+  for (int pol = (int) pol_start; pol <=(int) pol_end; pol++ )
   {
-    for (int ant = 0; ant < NUM_SEAVEYS; ant++) 
+    for (int ant = 0; ant < NUM_SEAVEYS; ant++)
     {
-      if (ring != AnitaRing::kNotARing && AnitaGeomTool::getRingFromAnt(ant) != ring) continue; 
-      vals[i++] = ( filtered ?  filteredGraphsByAntPol[pol][ant]->even() : rawGraphsByAntPol[pol][ant]->uneven())->getSumV2(); 
+      if (ring != AnitaRing::kNotARing && AnitaGeomTool::getRingFromAnt(ant) != ring) continue;
+      vals[i++] = ( filtered ?  filteredGraphsByAntPol[pol][ant]->even() : rawGraphsByAntPol[pol][ant]->uneven())->getSumV2();
     }
   }
 
-  std::nth_element(vals, vals + n/2, vals +n); 
+  std::nth_element(vals, vals + n/2, vals +n);
   return vals[n/2];
 }
 
 
 void FilteredAnitaEvent::getMedianSpectrum(TGraph * target, AnitaPol::AnitaPol_t pol, double frac) const
 {
-  target->Set(131); 
+  target->Set(131);
 
-  AnitaPol::AnitaPol_t pol_start =pol == AnitaPol::kVertical ? AnitaPol::kVertical : AnitaPol::kHorizontal;  
-  AnitaPol::AnitaPol_t pol_end = pol == AnitaPol::kHorizontal ? AnitaPol::kHorizontal : AnitaPol::kVertical; 
+  AnitaPol::AnitaPol_t pol_start =pol == AnitaPol::kVertical ? AnitaPol::kVertical : AnitaPol::kHorizontal;
+  AnitaPol::AnitaPol_t pol_end = pol == AnitaPol::kHorizontal ? AnitaPol::kHorizontal : AnitaPol::kVertical;
 
-//  memset(target->GetY(),0, target->GetN() * sizeof(double)); 
-  memcpy(target->GetX(),filteredGraphsByAntPol[pol_start][0]->power()->GetX(), target->GetN() * sizeof(double)); 
+//  memset(target->GetY(),0, target->GetN() * sizeof(double));
+  memcpy(target->GetX(),filteredGraphsByAntPol[pol_start][0]->power()->GetX(), target->GetN() * sizeof(double));
 
-  int n = NUM_SEAVEYS * (int(pol_end) - int(pol_start) +1); 
+  int n = NUM_SEAVEYS * (int(pol_end) - int(pol_start) +1);
 
 
-  double vals[n]; 
-  int i = 0; 
+  double vals[n];
+  int i = 0;
 
-  //Can paralellize this loop if it's helpful 
-  for (int j = 0; j < 131; j++) 
+  //Can paralellize this loop if it's helpful
+  for (int j = 0; j < 131; j++)
   {
-    i = 0; 
-    for (int pol = (int) pol_start; pol <=(int) pol_end; pol++ ) 
+    i = 0;
+    for (int pol = (int) pol_start; pol <=(int) pol_end; pol++ )
     {
-      for (int ant = 0; ant < NUM_SEAVEYS; ant++) 
+      for (int ant = 0; ant < NUM_SEAVEYS; ant++)
       {
-        //TODO: Is it faster just to put things into a set? 
-        vals[i++] = filteredGraphsByAntPol[pol][ant]->powerdB()->GetY()[j];  
+        //TODO: Is it faster just to put things into a set?
+        vals[i++] = filteredGraphsByAntPol[pol][ant]->powerdB()->GetY()[j];
       }
     }
-    std::nth_element(vals, vals + int(n*frac), vals +n); 
-    target->GetY()[j] = vals[int(n*frac)]; 
+    std::nth_element(vals, vals + int(n*frac), vals +n);
+    target->GetY()[j] = vals[int(n*frac)];
   }
 }
 
 
 void FilteredAnitaEvent::getAverageSpectrum(TGraph * target, AnitaPol::AnitaPol_t pol) const
 {
-  target->Set(131); 
+  target->Set(131);
 
-  AnitaPol::AnitaPol_t pol_start =pol == AnitaPol::kVertical ? AnitaPol::kVertical : AnitaPol::kHorizontal;  
-  AnitaPol::AnitaPol_t pol_end = pol == AnitaPol::kHorizontal ? AnitaPol::kHorizontal : AnitaPol::kVertical; 
+  AnitaPol::AnitaPol_t pol_start =pol == AnitaPol::kVertical ? AnitaPol::kVertical : AnitaPol::kHorizontal;
+  AnitaPol::AnitaPol_t pol_end = pol == AnitaPol::kHorizontal ? AnitaPol::kHorizontal : AnitaPol::kVertical;
 
-  memset(target->GetY(),0, target->GetN() * sizeof(double)); 
-  memcpy(target->GetX(),filteredGraphsByAntPol[pol_start][0]->power()->GetX(), target->GetN() * sizeof(double)); 
+  memset(target->GetY(),0, target->GetN() * sizeof(double));
+  memcpy(target->GetX(),filteredGraphsByAntPol[pol_start][0]->power()->GetX(), target->GetN() * sizeof(double));
 
-  int n = NUM_SEAVEYS * (int(pol_end) - int(pol_start) +1); 
+  int n = NUM_SEAVEYS * (int(pol_end) - int(pol_start) +1);
 
-  for (int pol = (int) pol_start; pol <=(int) pol_end; pol++ ) 
+  for (int pol = (int) pol_start; pol <=(int) pol_end; pol++ )
   {
-    for (int ant = 0; ant < NUM_SEAVEYS; ant++) 
+    for (int ant = 0; ant < NUM_SEAVEYS; ant++)
     {
-      for (int j = 0; j < 131; j++) 
+      for (int j = 0; j < 131; j++)
       {
-        target->GetY()[j] += filteredGraphsByAntPol[pol][ant]->power()->GetY()[j]/n;  
+        target->GetY()[j] += filteredGraphsByAntPol[pol][ant]->power()->GetY()[j]/n;
       }
     }
   }
 
-  for (int j = 0; j < 131; j++) 
+  for (int j = 0; j < 131; j++)
   {
-    target->GetY()[j] = 10 * TMath::Log10(target->GetY()[j]); 
+    target->GetY()[j] = 10 * TMath::Log10(target->GetY()[j]);
   }
 
 
@@ -227,41 +235,41 @@ void FilteredAnitaEvent::getAverageSpectrum(TGraph * target, AnitaPol::AnitaPol_
 
 void FilteredAnitaEvent::getMinMaxRatio(AnitaPol::AnitaPol_t pol, double * max_ratio, double * min_ratio, int* max_sector, int* min_sector, AnitaRing::AnitaRing_t ring1 , AnitaRing::AnitaRing_t ring2, int nth )  const
 {
-  
-  double max = 0; 
-  double min = 0; 
-  int imax = -1; 
-  int imin = -1; 
 
-  for (int i = 0; i < NUM_PHI; i++) 
+  double max = 0;
+  double min = 0;
+  int imax = -1;
+  int imin = -1;
+
+  for (int i = 0; i < NUM_PHI; i++)
   {
-    int ant1 = AnitaGeomTool::getAntFromPhiRing(i, ring1); 
-    int ant2 = AnitaGeomTool::getAntFromPhiRing(i, ring2); 
+    int ant1 = AnitaGeomTool::getAntFromPhiRing(i, ring1);
+    int ant2 = AnitaGeomTool::getAntFromPhiRing(i, ring2);
 
-//    printf("%d %d %d\n", pol, ant1,ant2); 
-//    printf("%p\n", rawGraphsByAntPol[pol][ant1]); 
-//    printf("%p\n", rawGraphsByAntPol[pol][ant2]); 
+//    printf("%d %d %d\n", pol, ant1,ant2);
+//    printf("%p\n", rawGraphsByAntPol[pol][ant1]);
+//    printf("%p\n", rawGraphsByAntPol[pol][ant2]);
     double peak1 = rawGraphsByAntPol[pol][ant1]->uneven()->pk2pk(nth,nth);
-    double peak2 = rawGraphsByAntPol[pol][ant2]->uneven()->pk2pk(nth,nth); 
+    double peak2 = rawGraphsByAntPol[pol][ant2]->uneven()->pk2pk(nth,nth);
 
-    double ratio = peak1/peak2; 
+    double ratio = peak1/peak2;
 
-    if ( imax < 0 || ratio > max ) 
+    if ( imax < 0 || ratio > max )
     {
-      imax = i; 
+      imax = i;
       max = ratio;
     }
-    if ( imax < 0 || ratio < min ) 
+    if ( imax < 0 || ratio < min )
     {
-      imin = i; 
+      imin = i;
       min = ratio;
     }
   }
 
-  if (max_ratio) *max_ratio = max; 
-  if (min_ratio) *min_ratio = min; 
-  if (max_sector) *max_sector= imax; 
-  if (min_sector) *min_sector= imin; 
+  if (max_ratio) *max_ratio = max;
+  if (min_ratio) *min_ratio = min;
+  if (max_sector) *max_sector= imax;
+  if (min_sector) *min_sector= imin;
 
 
 }
@@ -270,27 +278,25 @@ void FilteredAnitaEvent::getMinMaxRatio(AnitaPol::AnitaPol_t pol, double * max_r
 void FilteredAnitaEvent::plotSummary(TCanvas * ch, TCanvas * cv) const
 {
 
-  if (!ch) ch = new TCanvas("FilteredAnitaEvent_hpol","HPol", 1000,1000); 
-  if (!cv) cv = new TCanvas("FilteredAnitaEvent_vpol","VPol", 1000,1000); 
+  if (!ch) ch = new TCanvas("FilteredAnitaEvent_hpol","HPol", 1000,1000);
+  if (!cv) cv = new TCanvas("FilteredAnitaEvent_vpol","VPol", 1000,1000);
 
-  ch->Clear(); 
-  cv->Clear(); 
+  ch->Clear();
+  cv->Clear();
 
-  ch->Divide(8,NUM_SEAVEYS/8); 
-  cv->Divide(8,NUM_SEAVEYS/8); 
+  ch->Divide(8,NUM_SEAVEYS/8);
+  cv->Divide(8,NUM_SEAVEYS/8);
 
 
-  for (int i =0; i < NUM_SEAVEYS; i++) 
+  for (int i =0; i < NUM_SEAVEYS; i++)
   {
-    ch->cd(i+1); 
-    getRawGraph(i, AnitaPol::kHorizontal)->drawUneven("",1); 
-    getFilteredGraph(i, AnitaPol::kHorizontal)->drawEven("lsame",2); 
+    ch->cd(i+1);
+    getRawGraph(i, AnitaPol::kHorizontal)->drawUneven("",1);
+    getFilteredGraph(i, AnitaPol::kHorizontal)->drawEven("lsame",2);
 
-    cv->cd(i+1); 
-    getRawGraph(i, AnitaPol::kVertical)->drawUneven("",1); 
-    getFilteredGraph(i, AnitaPol::kVertical)->drawEven("lsame",2); 
+    cv->cd(i+1);
+    getRawGraph(i, AnitaPol::kVertical)->drawUneven("",1);
+    getFilteredGraph(i, AnitaPol::kVertical)->drawEven("lsame",2);
   }
 
 }
-
-
