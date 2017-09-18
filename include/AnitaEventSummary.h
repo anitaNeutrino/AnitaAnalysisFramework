5c26a5a6cf7123e316d9ee335450edd5c3c1fe74
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2017-09-18 01:10:33 -0500

    change most things to Double32_t, add n bot > top

--- include/AnitaEventSummary.h
+++ include/AnitaEventSummary.h
@@ -59,26 +59,26 @@ public:
   {
   public: 
     PointingHypothesis() : fContainer(NULL) { ; }
-    Double_t phi;  /// peak phi, degrees
-    Double_t theta; /// peak theta, degrees
-    Double_t value; /// peak value
-    Double_t snr; /// snr of peak
-    Double_t mapRMS; /// rms of interferometric map
-    Double_t mapHistoryVal; /// value of average of the peak location over the past 60 min-bias events
-    Double_t hwAngle; /// angle with respect to triggering phi sector
-    Double_t hwAngleXPol; /// angle with respect to triggering phi sector in opposite polarisation
-    Double_t latitude;/// on continent, or -9999 if doesn't intersect
-    Double_t longitude;/// on continent, or -9999 if doesn't intersect
-    Double_t altitude;/// on continent, or -9999 if doesn't intersect
-    Double_t distanceToSource; /// on continent, or -9999 if doesn't intersect
-    Double_t sigma_theta;  /// error on theta
-    Double_t sigma_phi;  /// error on phi
-    Double_t rho;  /// correlation coefficient between theta and phi
-    Double_t chisq; /// chisq/ndof of peak finding process, if available (otherwise zero)
-    Double_t theta_adjustment_needed; /// If an event barely missed the ground, it is useful to see the coordinates at which it would hit if theta adjustment by a small amount. This is the calculated small amount that leads to it hitting the ground. 
-    Double_t phi_separation; /// angular separation from higher value peak in same event. 1000 if highest value event (i.e. first hypothesis) 
-    Double_t dphi_rough;  /// phi - phi rough
-    Double_t dtheta_rough; /// theta - theta rough
+    Double32_t phi;  /// peak phi, degrees
+    Double32_t theta; /// peak theta, degrees
+    Double32_t value; /// peak value
+    Double32_t snr; /// snr of peak
+    Double32_t mapRMS; /// rms of interferometric map
+    Double32_t mapHistoryVal; /// value of average of the peak location over the past 60 min-bias events
+    Double32_t hwAngle; /// angle with respect to triggering phi sector
+    Double32_t hwAngleXPol; /// angle with respect to triggering phi sector in opposite polarisation
+    Double32_t latitude;/// on continent, or -9999 if doesn't intersect
+    Double32_t longitude;/// on continent, or -9999 if doesn't intersect
+    Double32_t altitude;/// on continent, or -9999 if doesn't intersect
+    Double32_t distanceToSource; /// on continent, or -9999 if doesn't intersect
+    Double32_t sigma_theta;  /// error on theta
+    Double32_t sigma_phi;  /// error on phi
+    Double32_t rho;  /// correlation coefficient between theta and phi
+    Double32_t chisq; /// chisq/ndof of peak finding process, if available (otherwise zero)
+    Double32_t theta_adjustment_needed; /// If an event barely missed the ground, it is useful to see the coordinates at which it would hit if theta adjustment by a small amount. This is the calculated small amount that leads to it hitting the ground. 
+    Double32_t phi_separation; /// angular separation from higher value peak in same event. 1000 if highest value event (i.e. first hypothesis) 
+    Double32_t dphi_rough;  /// phi - phi rough
+    Double32_t dtheta_rough; /// theta - theta rough
     Bool_t triggered; /// was this in a triggered phi sector? 
     Bool_t triggered_xpol; /// was this in a triggered xpol phi sector?
     Bool_t masked; /// was this in a masked phi sector?
@@ -125,47 +125,47 @@ public:
 
    public: 
     WaveformInfo() {; } 
-    Double_t snr; /// Signal to Noise of waveform 
-    Double_t peakHilbert; /// peak of hilbert envelope
-    Double_t peakVal;  /// peak value
-    Double_t xPolPeakVal;  /// Peak of xpol trace
-    Double_t xPolPeakHilbert;  /// Peak of xpol hilbert Envelope
+    Double32_t snr; /// Signal to Noise of waveform 
+    Double32_t peakHilbert; /// peak of hilbert envelope
+    Double32_t peakVal;  /// peak value
+    Double32_t xPolPeakVal;  /// Peak of xpol trace
+    Double32_t xPolPeakHilbert;  /// Peak of xpol hilbert Envelope
 
-    Double_t I,Q,U,V;  // Stokes Parameters
+    Double32_t I,Q,U,V;  // Stokes Parameters
 
 
-    Double_t totalPower;  /// Total power in waveform
-    Double_t totalPowerXpol;  /// Total power in xPol waveform
+    Double32_t totalPower;  /// Total power in waveform
+    Double32_t totalPowerXpol;  /// Total power in xPol waveform
 
     //spectrum info 
-    Double_t bandwidth[peaksPerSpectrum];  /// bandwidth of each peak (implementation defined, may not be comparable between analyses) 
-    Double_t peakFrequency[peaksPerSpectrum]; /// peak frequency of power spectrum 
-    Double_t peakPower[peaksPerSpectrum]; //power within +/- bandwidth of each peak 
-    Double_t spectrumSlope;  ///  Slope of line fit to spectrum (in log-space, so this is spectral-index) 
-    Double_t spectrumIntercept; /// Intercept of line fit to spectrum (in log-space) 
+    Double32_t bandwidth[peaksPerSpectrum];  /// bandwidth of each peak (implementation defined, may not be comparable between analyses) 
+    Double32_t peakFrequency[peaksPerSpectrum]; /// peak frequency of power spectrum 
+    Double32_t peakPower[peaksPerSpectrum]; //power within +/- bandwidth of each peak 
+    Double32_t spectrumSlope;  ///  Slope of line fit to spectrum (in log-space, so this is spectral-index) 
+    Double32_t spectrumIntercept; /// Intercept of line fit to spectrum (in log-space) 
 
     //Shape parameters, computed using hilbert envelope 
     // This should probably taken out into its own class 
-    Double_t riseTime_10_90;  /// Rise time of hilbert env from 10% to 90% of peak
-    Double_t riseTime_10_50;  /// Rise time of hilbert env from 10% to 50% of peak
-    Double_t fallTime_90_10;  /// Fall time of hilbert env from 90% to 10% of peak
-    Double_t fallTime_50_10;  /// Fall time of hilbert env from 50% to 10% of peak
-    Double_t width_50_50;   /// Width from first envelope crossing of 50 percent of peak to last 
-    Double_t width_10_10;  /// Width from first envelope crossing of 10 percent of peak to last 
-    Double_t power_10_10;  /// Power enclosed within 10_10 width
-    Double_t power_50_50;  /// Power enclosed within 50_50 width
-    Double_t peakTime;  // Time that peak hilbert env occurs
-    Double_t peakMoments[5];  // moments about Peak  (1st - 5th moments) 
+    Double32_t riseTime_10_90;  /// Rise time of hilbert env from 10% to 90% of peak
+    Double32_t riseTime_10_50;  /// Rise time of hilbert env from 10% to 50% of peak
+    Double32_t fallTime_90_10;  /// Fall time of hilbert env from 90% to 10% of peak
+    Double32_t fallTime_50_10;  /// Fall time of hilbert env from 50% to 10% of peak
+    Double32_t width_50_50;   /// Width from first envelope crossing of 50 percent of peak to last 
+    Double32_t width_10_10;  /// Width from first envelope crossing of 10 percent of peak to last 
+    Double32_t power_10_10;  /// Power enclosed within 10_10 width
+    Double32_t power_50_50;  /// Power enclosed within 50_50 width
+    Double32_t peakTime;  // Time that peak hilbert env occurs
+    Double32_t peakMoments[5];  // moments about Peak  (1st - 5th moments) 
 
-    Double_t impulsivityMeasure; /// A number that has something to do with how impulsive it is
-    Double_t narrowestWidths[5]; /// Narrowest width containing {10%, 20%, 30%, 40%, 50%} of the total power
+    Double32_t impulsivityMeasure; /// A number that has something to do with how impulsive it is
+    Double32_t narrowestWidths[5]; /// Narrowest width containing {10%, 20%, 30%, 40%, 50%} of the total power
 
     Int_t numAntennasInCoherent; /// Number of antennas used to make this
 
-    Double_t localMaxToMin; /// Largest value of local max to neighbouring local min (see Acclaim::RootTools::getLocalMaxToMin)
-    Double_t localMaxToMinTime; /// Time between local maxima and minima +ve means max is before min, -ve means min is before max
-    Double_t globalMaxToMin; /// Difference between maximum and minimum voltage
-    Double_t globalMaxToMinTime; /// Time between maximum and minimum volts, +ve means max is before min, -ve means min is before max 
+    Double32_t localMaxToMin; /// Largest value of local max to neighbouring local min (see Acclaim::RootTools::getLocalMaxToMin)
+    Double32_t localMaxToMinTime; /// Time between local maxima and minima +ve means max is before min, -ve means min is before max
+    Double32_t globalMaxToMin; /// Difference between maximum and minimum voltage
+    Double32_t globalMaxToMinTime; /// Time between maximum and minimum volts, +ve means max is before min, -ve means min is before max 
 
 
     //some utilities for polarization info
@@ -179,7 +179,7 @@ public:
     inline double skewness(){return standardizedPeakMoment(3);}
     inline double kurtosis(){return standardizedPeakMoment(4);}
 
-    ClassDefNV(WaveformInfo, 10);
+    ClassDefNV(WaveformInfo, 11);
 
     // private:
     //  mutable AnitaEventSummary* fContainer; //! Disgusting hack
@@ -196,10 +196,10 @@ public:
     /// Correct indices are set in the AnitaEventSummary constructor
     ChannelInfo() : pol(AnitaPol::kNotAPol), ant(-1) {; }
 
-    Double_t rms;
-    Double_t avgPower;
-    Double_t snr; /// Signal to Noise of waveform 
-    Double_t peakHilbert; /// peak of hilbert envelope
+    Double32_t rms;
+    Double32_t avgPower;
+    Double32_t snr; /// Signal to Noise of waveform 
+    Double32_t peakHilbert; /// peak of hilbert envelope
 
     Double_t getPhi() const;
     inline Int_t getAnt() const {return ant;}                // could add some errors on -1 here...
@@ -214,7 +214,7 @@ public:
     AnitaPol::AnitaPol_t     pol; //! DOES NOT PERSIST IN ROOT! the polarization
     Int_t                    ant; //! DOES NOT PERSIST IN ROOT! the antenna
 
-    ClassDefNV(ChannelInfo, 3);
+    ClassDefNV(ChannelInfo, 4);
   }; 
 
 
@@ -257,20 +257,23 @@ public:
      *  The first element is the total, and then the next are by ring 
      *  So to get the top ring, do 1 + AnitaRing::kTopRing, etc. 
      */
-    Double_t meanPower[1+AnitaRing::kNotARing];
-    Double_t medianPower[1+AnitaRing::kNotARing];
-    Double_t meanPowerFiltered[1+AnitaRing::kNotARing];
-    Double_t medianPowerFiltered[1+AnitaRing::kNotARing];
+    Double32_t meanPower[1+AnitaRing::kNotARing];
+    Double32_t medianPower[1+AnitaRing::kNotARing];
+    Double32_t meanPowerFiltered[1+AnitaRing::kNotARing];
+    Double32_t medianPowerFiltered[1+AnitaRing::kNotARing];
 
-    Double_t maxBottomToTopRatio[AnitaPol::kNotAPol]; 
+    Double32_t maxBottomToTopRatio[AnitaPol::kNotAPol]; 
     int maxBottomToTopRatioSector[AnitaPol::kNotAPol]; 
-    Double_t minBottomToTopRatio[AnitaPol::kNotAPol]; 
+    Double32_t minBottomToTopRatio[AnitaPol::kNotAPol]; 
     int minBottomToTopRatioSector[AnitaPol::kNotAPol]; 
+
+
+    Int_t nSectorsWhereBottomExceedsTop; 
 
     /** The fraction of nearby events that are payload blasts */ 
     Double_t blastFraction; 
 
-    ClassDefNV(EventFlags,9);
+    ClassDefNV(EventFlags,10);
   };
 
 
@@ -285,12 +288,12 @@ public:
   {
    public:
     SourceHypothesis() { reset(); }
-    Double_t theta;
-    Double_t phi;
-    Double_t distance;
+    Double32_t theta;
+    Double32_t phi;
+    Double32_t distance;
 
-    Double_t mapValue[NUM_POLS];  ///what the instantaneous map value is at this source hypothesis
-    Double_t mapHistoryVal[NUM_POLS]; /// a history of the interferometric map value for the source location
+    Double32_t mapValue[NUM_POLS];  ///what the instantaneous map value is at this source hypothesis
+    Double32_t mapHistoryVal[NUM_POLS]; /// a history of the interferometric map value for the source location
 
     void reset(); /// sets all the values to nonsense.  Sorry, mapHistoryVal means this is in source now 
       
@@ -428,7 +431,7 @@ public:
 
 
 
-  ClassDefNV(AnitaEventSummary, 28);
+  ClassDefNV(AnitaEventSummary, 29);
 };
 
 #endif 
