f3181ff2f054507430a4d492ea51ab85fa8c2334
aludez <drewbaludwig@gmail.com>
2017-11-30 14:50:08 -0600

    event summary back as it was

--- include/AnitaEventSummary.h
+++ include/AnitaEventSummary.h
@@ -177,7 +177,6 @@ class AnitaEventSummary : public TObject
     Double_t peakMoments[5];  /// moments about Peak  (1st - 5th moments)
 
     Double_t impulsivityMeasure; //[-1,1, 16]  /// A number that has something to do with how impulsive it is
-    Double_t bandwidthMeasure; /// A number that has something to do with how broadband it is (0 to 1, higher = more broadband)
     Double_t fracPowerWindowBegins[numFracPowerWindows]; //[0,128,16] /// Narrowest width containing {10%, 20%, 30%, 40%, 50%} of the total power
     Double_t fracPowerWindowEnds[numFracPowerWindows]; //[0,128,16] /// Narrowest width containing {10%, 20%, 30%, 40%, 50%} of the total power
 
@@ -204,7 +203,7 @@ class AnitaEventSummary : public TObject
     Double_t fracPowerWindowIntercept() const;
     Double_t fracPowerWindowChisquare() const;
 
-    ClassDefNV(WaveformInfo, 17);
+    ClassDefNV(WaveformInfo, 16);
 
    private:
     friend class AnitaEventSummary;
