0f2a6c7f9c9f0ca240b8107e632ad6028a05ac5d
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2018-01-01 23:32:41 -0600

    updated sensitivity calculator that also compiles properly

--- include/SensitivityCalculator.h
+++ include/SensitivityCalculator.h
@@ -1,12 +1,11 @@
 #ifndef SENSITIVITY_CALCULATOR_HH
 #define SENSITIVITY_CALCULATOR_HH
 
-#ifdef USE_ROOSTATS
-#include "RooWorkspace.h" 
-#endif 
  
 
+class TH1; 
 class TGraphErrors; 
+class RooWorkspace; 
 
 /** Sensitivity calculator ...
  *
@@ -15,7 +14,7 @@ class TGraphErrors;
  * This can easily be generalized to different efficiency / background models pretty easily (although, eventually you'll just be implementing an interface to RooStats) . 
  *
  * This can either use the ``large-sample'' profile likelihood method as described in: 
- * https://arxiv.org/abs/1505.07027 or a profile-likelihood-weighted FC-like method (which requires a large amount of MC) 
+ * https://arxiv.org/abs/1505.07027 or a profile-likelihood-weighted FC-like method (which requires a large amount of MC and is much slower) 
  * 
  * Cosmin Deaconu <cozzyd@kicp.uchicago.edu> 
  */
@@ -27,22 +26,26 @@ class SensitivityCalculator
 
   public: 
 
-    SensitivityCalculator(double CL = 0.9,bool FC = true) ;
+    SensitivityCalculator(double CL = 0.9,bool FC = false) ;
+    virtual ~SensitivityCalculator(); 
       
 
     void setEfficiency(double mean, double sigma); 
-    void setThermalBackground(int nobserved, double bg_to_sig_ratio); 
-    void setAnthroBackground(int nobserved, double bg_to_sig_ratio); 
-    void getLimit(int nobs, double * lower = 0, double * upper = 0); 
+    void setThermalBackground(int nobserved, double mean_bg_to_sig_ratio); 
+    void setAnthroBackground(int nobserved, double mean_bg_to_sig_ratio, double sigma_bg_to_sig_ratio); 
+    //returns sensitivity 
+    double getLimit(int nobs, double * lower = 0, double * upper = 0); 
     TGraphErrors * confidenceBands(int start = 0, int stop = 20); 
+    TH1*  histNObserved(double S); 
+    TH1*  histBAnthro(); 
+    TH1*  histBThermal(); 
+    TH1*  histBTotal(); 
   private: 
 
     double cl;
     bool use_fc;
 
-#ifdef USE_ROOSTATS
-    RooWorkspace w; 
-#endif
+    RooWorkspace *  w; 
 
 };
 
--- src/SensitivityCalculator.cc
+++ src/SensitivityCalculator.cc
@@ -1,18 +1,27 @@
 #include "SensitivityCalculator.h" 
 #include "TGraphErrors.h" 
 #include "TAxis.h" 
+#include "TH1.h" 
 
 #ifdef USE_ROOSTATS
+#include "RooWorkspace.h" 
 #include "RooStats/FeldmanCousins.h" 
+#include "RooStats/HypoTestResult.h" 
 #include "RooStats/ProfileLikelihoodCalculator.h" 
 #endif
 
 
+SensitivityCalculator::~SensitivityCalculator() 
+{
+#ifdef USE_ROOSTATS
+
+  delete w; 
+#endif
+}
+
+
 
 SensitivityCalculator::SensitivityCalculator(double CL, bool FC) 
-#ifdef USE_ROOSTATS
-: w("w")
-#endif 
 {
   cl = CL;
   use_fc = FC; 
@@ -23,48 +32,72 @@ SensitivityCalculator::SensitivityCalculator(double CL, bool FC)
 
 
 #else
+  w = new RooWorkspace("w"); 
 
-  w.factory("Poisson::obsModel(nobs[0,0,50], sum(prod(s[1,0,50], eff[0.7,0,1]),banthro[1,0,50],bthermal[1,0,50]))"); 
-  w.factory("Poisson::thermalModel(nthermal[1,0,10], prod::taubthermal(bthermal, tauthermal[3,0,10]))");
-  w.factory("Poisson::anthroModel(nanthro[1,0,10], prod::taubanthro(banthro, tauanthro[2,0,10]))");
-  w.factory("Gaussian::effModel(mean_eff[0.7,0,1], eff, sigma_eff[0.05,0,0.2])"); 
+  
+  /* We observe from a poisson distribution, with a mean of e * s + banthro + bthermal */ 
+  w->factory("Poisson::obsModel(nobs[0,0,20], sum(prod(s[1,0,50], eff[0.7,0,1]),banthro[1,0,30],bthermal[0.3,0,30]))"); 
 
-  w.factory("PROD::model(obsModel,thermalModel,anthroModel,effModel)"); 
+  /*We estimate bthermal from the counts in a sideband that's tauthermal size the signal region, so nthermal (what we observe) is poisson distributed with a mean of tauthermal * bthermal */ 
+  w->factory("Poisson::thermalModel(nthermal[1,0,30], prod::taubthermal(bthermal, tauthermal[3,0,10]))");
 
-  w.var("tauthermal")->setConstant(); 
-  w.var("tauanthro")->setConstant(); 
-  w.var("nthermal")->setConstant(); 
-  w.var("nanthro")->setConstant(); 
-  w.var("mean_eff")->setConstant(); 
-  w.var("sigma_eff")->setConstant(); 
+  /*We estimate banthro from the counts in a sideband that's tauanthro size the signal region, so nanthro (what we observe) is poisson distributed with a mean of tauanthro * banthro */ 
+  w->factory("Poisson::anthroModel(nanthro[1,0,30], prod::taubanthro(banthro, tauanthro[1,0,10]))");
 
-  w.defineSet("poi","s"); //parameter of interest
-  w.defineSet("nuis","bthermal,banthro,eff"); //nuisance parameters 
-  w.defineSet("gobs","nthermal,nanthro,tauthermal,tauanthro,mean_eff,sigma_eff"); //"global observed parameters" 
-  w.defineSet("obs","nobs"); //"global observed parameters" 
+  /*Errors on tau anthro */ 
+  w->factory("Gaussian::anthroTauErrorModel(mean_tauanthro[1,0,10],tauanthro, sigma_tau_anthro[0.1,0,1])"); 
+
+  /* We assign a systematic error (gaussian) to our analysis efficiency */ 
+  w->factory("Gaussian::effModel(mean_eff[0.7,0,1], eff, sigma_eff[0.05,0,0.2])"); 
+
+  /* The likelihood  model is the product of all of the distributions */ 
+  w->factory("PROD::model(obsModel,thermalModel,anthroModel,effModel, anthroTauErrorModel)"); 
+
+  w->factory("SUM:total_bg(banthro,bthermal)"); 
+
+
+
+
+  /* Set all auxilliary observables to constants */ 
+  w->var("tauthermal")->setConstant(); 
+  w->var("mean_tauanthro")->setConstant(); 
+  w->var("sigma_tau_anthro")->setConstant(); 
+  w->var("nthermal")->setConstant(); 
+  w->var("nanthro")->setConstant(); 
+  w->var("mean_eff")->setConstant(); 
+  w->var("sigma_eff")->setConstant(); 
+
+  w->defineSet("poi","s"); //parameter of interest
+  w->defineSet("nuis","bthermal,banthro,eff,tauanthro"); //nuisance parameters 
+  w->defineSet("gobs","nthermal,nanthro,tauthermal,mean_tauanthro,sigma_tau_anthro,mean_eff,sigma_eff"); //"global observed parameters" 
+  w->defineSet("obs","nobs"); //"main observed parameters" 
+  w->defineSet("sens","bthermal,banthro,eff,nobs,tauanthro"); 
+  w->defineSet("bkg_anthro","banthro,tauanthro"); 
+  w->defineSet("bkg_thermal","bthermal"); 
+  w->defineSet("bkg_total","banthro,tauanthro,bthermal"); 
 
   setEfficiency(0.7,0.05); 
   setThermalBackground(1,3); 
-  setAnthroBackground(0,0.3); 
+  setAnthroBackground(1,1,0.1); 
 #endif
 
 }  
 
-void SensitivityCalculator::SensitivityCalculator::setThermalBackground(int nobs, double tau)
+void SensitivityCalculator::SensitivityCalculator::setThermalBackground(int nobs, double tau) 
 {
 #ifdef USE_ROOSTATS
-
-  w.var("tauthermal")->setVal(tau); 
-  w.var("nthermal")->setVal(nobs); 
+  w->var("tauthermal")->setVal(tau); 
+  w->var("nthermal")->setVal(nobs); 
 #endif
 }
 
 
-void SensitivityCalculator::SensitivityCalculator::setAnthroBackground(int nobs, double tau)
+void SensitivityCalculator::SensitivityCalculator::setAnthroBackground(int nobs, double tau, double sigma_tau)
 {
 #ifdef USE_ROOSTATS
-  w.var("tauanthro")->setVal(tau); 
-  w.var("nanthro")->setVal(nobs); 
+  w->var("mean_tauanthro")->setVal(tau); 
+  w->var("sigma_tau_anthro")->setVal(sigma_tau); 
+  w->var("nanthro")->setVal(nobs); 
 #endif
 
 }
@@ -72,27 +105,39 @@ void SensitivityCalculator::SensitivityCalculator::setAnthroBackground(int nobs,
 void SensitivityCalculator::SensitivityCalculator::setEfficiency(double mean, double sigma)
 {
 #ifdef USE_ROOSTATS
-  w.var("mean_eff")->setVal(mean); 
-  w.var("sigma_eff")->setVal(sigma); 
+  if (sigma == 0) 
+  {
+    w->var("eff")->setVal(mean); 
+    w->var("eff")->setConstant(true); 
+
+  }
+  else
+  {
+    w->var("eff")->setConstant(false); 
+    w->var("mean_eff")->setVal(mean); 
+    w->var("sigma_eff")->setVal(sigma); 
+  }
 #endif
 }
 
 
-void SensitivityCalculator::getLimit(int nobs, double * lower, double * upper) 
+
+double SensitivityCalculator::getLimit(int nobs, double * lower, double * upper) 
 {
 #ifdef USE_ROOSTATS
-  w.var("nobs")->setVal(nobs); 
-  RooDataSet data("data","",*w.var("nobs")); 
-  data.add(*w.var("nobs")); 
+  w->var("s")->setConstant(false); 
+  w->var("nobs")->setVal(nobs); 
+  RooDataSet data("data","",*w->var("nobs")); 
+  data.add(*w->var("nobs")); 
 
   //generate model with what we've observed 
-  RooStats::ModelConfig m("limits"); 
-  m.SetWorkspace(w); 
-  m.SetPdf(*w.pdf("model")); 
-  m.SetObservables(*w.var("nobs")); 
-  m.SetGlobalObservables(*w.set("gobs")); 
-  m.SetParametersOfInterest(*w.set("poi")); 
-  m.SetNuisanceParameters(*w.set("nuis")); 
+  RooStats::ModelConfig m("mod"); 
+  m.SetWorkspace(*w); 
+  m.SetPdf(*w->pdf("model")); 
+  m.SetObservables(*w->var("nobs")); 
+  m.SetGlobalObservables(*w->set("gobs")); 
+  m.SetParametersOfInterest(*w->set("poi")); 
+  m.SetNuisanceParameters(*w->set("nuis")); 
 
   if (use_fc)
   {
@@ -103,17 +148,21 @@ void SensitivityCalculator::getLimit(int nobs, double * lower, double * upper)
     fc.SetNBins(501);
     RooStats::PointSetInterval * ci = fc.GetInterval();  
 
-    if (upper) *upper=ci->UpperLimit(*w.var("s")); 
-    if (lower) *lower=ci->LowerLimit(*w.var("s")); 
+    if (upper) *upper=ci->UpperLimit(*w->var("s")); 
+    if (lower) *lower=ci->LowerLimit(*w->var("s")); 
+    return 0;
   }
   else
   {
     //use profile likelihood
     RooStats::ProfileLikelihoodCalculator pl(data, m); 
     pl.SetConfidenceLevel(cl); 
+    pl.SetNullParameters(*w->set("poi")); 
     RooStats::LikelihoodInterval * ci = pl.GetInterval(); 
-    if (upper) *upper=ci->UpperLimit(*w.var("s")); 
-    if (lower) *lower=ci->LowerLimit(*w.var("s")); 
+    pl.GetHypoTest()->Print(); 
+    if (upper) *upper=ci->UpperLimit(*w->var("s")); 
+    if (lower) *lower=ci->LowerLimit(*w->var("s")); 
+    return 0; 
   }
 #endif
 
@@ -145,6 +194,70 @@ TGraphErrors * SensitivityCalculator::confidenceBands(int start, int stop)
 }
 
 
+TH1 * SensitivityCalculator::histBAnthro() 
+{
+#ifdef USE_ROOSTATS
+  RooDataSet * data = w->pdf("model")->generate(*w->set("bkg_anthro"),10000); 
+  TH1 * hist =  data->createHistogram("B_{anthro}", *w->var("banthro")); 
+  hist->Scale(1./10000); 
+  delete data; 
+  return hist; 
+#else 
+  return 0; 
+#endif
+}
+
+TH1 * SensitivityCalculator::histBThermal() 
+{
+#ifdef USE_ROOSTATS
+  RooDataSet * data = w->pdf("model")->generate(*w->set("bkg_thermal"),10000); 
+  TH1 * hist =  data->createHistogram("B_{thermal}", *w->var("bthermal")); 
+  hist->Scale(1./10000); 
+  delete data; 
+  return hist; 
+#else 
+  return 0; 
+#endif 
+}
+
+TH1 * SensitivityCalculator::histBTotal() 
+{
+  TH1 * banthro = histBAnthro(); 
+  TH1 * bthermal = histBThermal(); 
+  TH1 *  btotal = new TH1D("total", "Total", 100,0,20); 
+
+  for (int i = 0; i < 10000; i++)
+  {
+    btotal->Fill(banthro->GetRandom() + bthermal->GetRandom()); 
+  }
+  btotal->Scale(1./btotal->Integral()); 
+  delete banthro; 
+  delete bthermal; 
+  return btotal; 
+}
+
+
+
+
+TH1 * SensitivityCalculator::histNObserved(double S)
+{
+#ifdef USE_ROOSTATS
+
+  w->var("s")->setVal(S); 
+  w->var("s")->setConstant(true); 
+  RooDataSet * data = w->pdf("model")->generate(*w->set("sens"),10000); 
+//  data->Print(); 
+  TString str;
+  str.Form("observed_s_%g",S); 
+  TH1 * hist =  data->createHistogram(str.Data(), *w->var("nobs"),RooFit::Binning(20,0,20)); 
+  hist->Scale(1./10000); 
+  delete data; 
+  w->var("s")->setConstant(false);
+  return hist; 
+#else
+  return 0;
+#endif
+}
 
 
 
